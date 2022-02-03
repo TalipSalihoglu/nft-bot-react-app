87b08282c88d96706b3378e1b5ca51f42f70b5ae
Abdullah-Koroglu <abdullah34koroglu@gmail.com>
2022-02-03 18:29:39 +0300

    try to fix cors error

--- src/components/Bot/Nft/index.js
+++ src/components/Bot/Nft/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const fetcheden = (eden_collection) => {
     return new Promise((resolve) => {
-        fetch(`${process.env.REACT_APP_API_ENDPOINT}getCollectionEscrowStats/`+eden_collection, {
+        fetch(eden_collection, {
             mode: 'cors',
             headers: {
               'Access-Control-Allow-Origin':'*'
@@ -25,7 +25,7 @@ function play(){
 const fetcheden2 = (eden_collection) => {
     return new Promise((resolve) => {
         const query = `{"$match":{"collectionSymbol":"${eden_collection}"},"$sort":{"createdAt":-1},"$skip":0,"$limit":2000}`;
-        fetch(`${process.env.REACT_APP_API_ENDPOINT}getListedNFTsByQuery?q=${query}`,{
+        fetch(`getListedNFTsByQuery?q=${query}`,{
             mode: 'cors',
             headers: {
               'Access-Control-Allow-Origin':'*'
